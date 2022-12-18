2ad98971220fadb09e6f0bd0758f1395f42fa014
Sharad.patel <sharad.patel@letsnurture.co.in>
2022-12-18 13:12:22 +0530

    Show files

--- index.js
+++ index.js
@@ -52,17 +52,32 @@ const http = require("http");
 
 // =================================== 12.Input From Command Line ==============================
 // console.log(process.argv["2"]);
-const fs = require("fs");
+// const fs = require("fs");
 
-const input = process.argv;
+// const input = process.argv;
 
-// fs.writeFileSync(input["2"], input["3"]);
-// fs.unlinkSync(input["2"]);
+// // fs.writeFileSync(input["2"], input["3"]);
+// // fs.unlinkSync(input["2"]);
 
-if (input["2"] == "add") {
-  fs.writeFileSync(input["3"], input["4"]); // Create File & Write
-} else if (input["2"] == "remove") {
-  fs.unlinkSync(input["3"]); // Delete File
-} else {
-  console.log("Invalid Input");
-}
+// if (input["2"] == "add") {
+//   fs.writeFileSync(input["3"], input["4"]); // Create File & Write
+// } else if (input["2"] == "remove") {
+//   fs.unlinkSync(input["3"]); // Delete File
+// } else {
+//   console.log("Invalid Input");
+// }
+// ================================ 13. Show Files List =======================================
+
+// const fs = require("fs");
+// const path = require("path");
+// const dirPath = path.join(__dirname, "files"); // upto files folder path
+// // console.log(dirPath);
+// for (i = 0; i < 2; i++) {
+//   fs.writeFileSync(dirPath + "/Hello_" + i + ".txt", "Hello world"); //Make files in dirPath
+// }
+
+// fs.readdir(dirPath, (err, files) => {
+//   files.forEach((item) => {
+//     console.log(item); //List file Path
+//   });
+// });
