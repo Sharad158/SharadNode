d991ec03a2153bbab41156ceec07896f50b1a703
Sharad.patel <sharad.patel@letsnurture.co.in>
2022-12-18 13:51:27 +0530

    File crud

--- index.js
+++ index.js
@@ -81,3 +81,26 @@ const http = require("http");
 //     console.log(item); //List file Path
 //   });
 // });
+
+// ================================ 14. CRUD File Asyncronus ========================================
+
+// const fs = require("fs");
+// const path = require("path");
+// const dirPath = path.join(__dirname, "crud");
+// const filepath = `${dirPath}/apple.txt`;
+
+// fs.writeFileSync(filepath, "this is apple.");
+
+// fs.readFile(filepath, "utf8", (err, item) => {
+//   console.log(item);
+// });
+
+// fs.appendFile(filepath, " it is sweet", (err) => {
+//   if (!err) console.log("file updated");
+// });
+
+// fs.rename(filepath, `${dirPath}/fruit.txt`, (err) => {
+//   if (!err) console.log("file name is updated");
+// });
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`);
