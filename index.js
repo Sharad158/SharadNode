b5da5fc03265b959054c149a64b06a60a1891b67
Sharad Personal <sharadpatel158@gmail.com>
2023-01-04 19:09:47 +0530

    Nake Html Pages

--- index.js
+++ index.js
@@ -177,3 +177,18 @@ const http = require("http");
 // });
 
 // app.listen(3000);
+
+// ========================================= 21V. Make HTML Pages =========================================================
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// const publicPath = path.join(__dirname,'public');
+
+// // console.log(__dirname);
+// // console.log(publicPath);
+
+// app.use(express.static(publicPath));
+
+// app.listen(3000);
