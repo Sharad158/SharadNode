2b9689aea4903338802dd1c45ffd0e0f076e4ddb
Sharad Patel <sharadp.letsnurture@gmail.com>
2023-04-04 09:59:56 +0530

    promise hadle using then & async await

--- fetchdata.js
+++ fetchdata.js
@@ -1,4 +1,12 @@
 const dbConnect = require('./database');
+// 1. Promise Handle Using then 
+// dbConnect().then((resp) => {
+//     resp.find().toArray().then((res) => {
+//         console.log(res);
+//     });
+// });
+
+// 2. Promise Handle Using async & await
 
 const main = async () => {
     let data = await dbConnect();
